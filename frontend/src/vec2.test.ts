c454f2385e6aa3d6671c5fc4595a8173d6e6d0d2
scarf <greenscarf005@gmail.com>
2022-11-23 17:13:03 +0900

    feat: Vec2, Pos, Size 클래스 추가 (#78)
    * feat: Vec2, Pos, Size 클래스 추가
    
    * test: Vec2, Pos, Size 기본 테스트 추가
    
    * test: equal() 메소드 테스트 추가
    
    * refactor: `equal` -> `eq`
    
    좀 더 읽기 편한 듯

---
+++ frontend/src/vec2.test.ts
@@ -0,0 +1,54 @@
+import { pos, Pos, size, Size, vec2, Vec2 } from './vec2'
+import { describe, expect, test } from 'vitest'
+
+type Create = 'Vec2' | 'Pos' | 'Size'
+const create =
+  (type: Create = 'Vec2') =>
+  (a: number, b: number) => {
+    switch (type) {
+      case 'Vec2':
+        return vec2(a, b)
+      case 'Pos':
+        return pos(a, b)
+      case 'Size':
+        return size(a, b)
+    }
+  }
+
+describe.each(['Vec2', 'Pos', 'Size'] as Create[])('%s 메소드', type => {
+  const cons = create(type)
+  const a = cons(1, 2)
+  const b = cons(3, 4)
+
+  test('neg', () => expect(a.neg()).toEqual(cons(-1, -2)))
+
+  test('add', () => expect(a.add(b)).toEqual(cons(4, 6)))
+  test('sub', () => expect(a.sub(b)).toEqual(cons(-2, -2)))
+
+  describe('mul', () => {
+    test(`with Vec2`, () => expect(a.mul(b)).toEqual(cons(3, 8)))
+    test(`with number`, () => expect(a.mul(2)).toEqual(cons(2, 4)))
+  })
+
+  describe('div', () => {
+    test('with cons', () => expect(a.div(b)).toEqual(cons(1 / 3, 0.5)))
+    test('with number', () => expect(a.div(2)).toEqual(cons(0.5, 1)))
+  })
+})
+
+describe('Size와 Pos 사이', () => {
+  const a = size(1, 1)
+  const b = pos(1, 1)
+  test('Size + Pos', () => {
+    expect(a.add(b)).toEqual(vec2(2, 2))
+  })
+  test('Size == Pos', () => {
+    expect(a).toEqual(b)
+    expect(a.eq(b)).toBe(true)
+  })
+
+  // @ts-expect-error
+  test('Size.x는 없음', () => expect(a.x).toBeUndefined())
+  // @ts-expect-error
+  test('Pos.w는 없음', () => expect(b.w).toBeUndefined())
+})
---
+++ frontend/src/vec2.ts
@@ -0,0 +1,103 @@
+export class Vec2 {
+  v: [number, number]
+  constructor(a: number, b: number) {
+    this.v = [a, b]
+  }
+
+  static zero() {
+    return new Vec2(0, 0)
+  }
+  static one() {
+    return new Vec2(1, 1)
+  }
+
+  neg() {
+    return new Vec2(-this.v[0], -this.v[1])
+  }
+  eq(other: Vec2): boolean {
+    return this.v[0] === other.v[0] && this.v[1] === other.v[1]
+  }
+  add(other: Vec2) {
+    return new Vec2(this.v[0] + other.v[0], this.v[1] + other.v[1])
+  }
+  sub(other: Vec2) {
+    return new Vec2(this.v[0] - other.v[0], this.v[1] - other.v[1])
+  }
+  /**
+   * @example
+   * vec2(1, 2).mul(3, 4) // => vec2(3, 8)
+   */
+  mul(other: Vec2): Vec2
+  /**
+   * @example
+   * vec2(1, 2).mul(2) // => vec2(2, 4)
+   */
+  mul(scalar: number): Vec2
+  mul(other: Vec2 | number) {
+    switch (typeof other) {
+      case 'number':
+        return new Vec2(this.v[0] * other, this.v[1] * other)
+      default:
+        return new Vec2(this.v[0] * other.v[0], this.v[1] * other.v[1])
+    }
+  }
+  /**
+   * @example
+   * vec2(3, 8).div(3, 4) // => vec2(1, 2)
+   */
+  div(other: Vec2): Vec2
+  /**
+   * @example
+   * vec2(2, 4).div(2) // => vec2(1, 2)
+   */
+  div(scalar: number): Vec2
+  div(other: Vec2 | number) {
+    switch (typeof other) {
+      case 'number':
+        return new Vec2(this.v[0] / other, this.v[1] / other)
+      default:
+        return new Vec2(this.v[0] / other.v[0], this.v[1] / other.v[1])
+    }
+  }
+}
+
+export class Pos extends Vec2 {
+  constructor(x: number, y: number) {
+    super(x, y)
+  }
+  get x() {
+    return this.v[0]
+  }
+  get y() {
+    return this.v[1]
+  }
+  set x(x: number) {
+    this.v[0] = x
+  }
+  set y(y: number) {
+    this.v[1] = y
+  }
+}
+
+export class Size extends Vec2 {
+  constructor(width: number, height: number) {
+    super(width, height)
+  }
+  get w() {
+    return this.v[0]
+  }
+  get h() {
+    return this.v[1]
+  }
+  set w(width: number) {
+    this.v[0] = width
+  }
+  set h(height: number) {
+    this.v[1] = height
+  }
+}
+
+type Cons = (a: number, b: number) => Vec2
+export const vec2: Cons = (a, b) => new Vec2(a, b)
+export const pos: Cons = (a, b) => new Pos(a, b)
+export const size: Cons = (a, b) => new Size(a, b)
