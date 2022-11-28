b4180aedfc2bfd9fec13feeaedca4acb91eac133
jeonhyun <jeonhyun@student.42seoul.kr>
2022-11-28 16:36:40 +0900

    style : 네비게이션 아이콘 추가 및 CSS 개선

--- src/components/users/Navigation.tsx
+++ src/components/users/Navigation.tsx
@@ -1,24 +1,44 @@
-import { Button, Popover } from '@mantine/core'
+import { Button, Popover, Image } from '@mantine/core'
 import { Logo } from 'components/common'
 import { storeVisibleAtom } from 'Atoms'
-import { useSetAtom } from 'jotai'
+import { useAtom, useSetAtom } from 'jotai'
 import { useState } from 'react'
+import shop from 'assets/shop.png'
+import user from 'assets/user.png'
 
 export const Navigation = () => {
-  const setStoreVisible = useSetAtom(storeVisibleAtom)
+  const [storeVisible, setStoreVisible] = useAtom(storeVisibleAtom)
   const [opened, setOpened] = useState(false)
 
-  const tmpStyle: React.CSSProperties = {
+  const navStyle: React.CSSProperties = {
     background: '#252525',
+    display: 'flex',
+  }
+  const iconStyle: React.CSSProperties = {
+    width: '40px',
+    height: '40px',
+    filter: 'invert(100%)',
   }
 
   return (
-    <nav style={tmpStyle}>
+    <nav style={navStyle}>
       <Logo />
-      <Button onClick={() => setStoreVisible(true)}>스토어</Button>
+      <Image
+        src={shop}
+        style={iconStyle}
+        onClick={() => setStoreVisible(storeVisible ? false : true)}
+      >
+        스토어
+      </Image>
       <Popover opened={opened} onChange={setOpened}>
         <Popover.Target>
-          <Button onClick={() => setOpened(opened ? false : true)}>유저</Button>
+          <Image
+            src={user}
+            style={{ ...iconStyle, marginLeft: 'auto' }}
+            onClick={() => setOpened(opened ? false : true)}
+          >
+            유저
+          </Image>
         </Popover.Target>
         <Popover.Dropdown>
           <Button>로그아웃</Button>
