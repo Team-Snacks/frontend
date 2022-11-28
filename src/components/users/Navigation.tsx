c0e00d856fcc4a233a0e3065f242c9529e96700b
jeonhyun <jeonhyun@student.42seoul.kr>
2022-11-28 16:04:34 +0900

    feat: 유저 모달 추가, Logo 스타일 수정

--- src/components/users/Navigation.tsx
+++ src/components/users/Navigation.tsx
@@ -1,17 +1,29 @@
-import { Button } from '@mantine/core'
+import { Button, Popover } from '@mantine/core'
 import { Logo } from 'components/common'
 import { storeVisibleAtom } from 'Atoms'
 import { useSetAtom } from 'jotai'
+import { useState } from 'react'
 
 export const Navigation = () => {
   const setStoreVisible = useSetAtom(storeVisibleAtom)
-  const tmpStyle: React.CSSProperties = { background: '#aaffff' }
+  const [opened, setOpened] = useState(false)
+
+  const tmpStyle: React.CSSProperties = {
+    background: '#252525',
+  }
 
   return (
     <nav style={tmpStyle}>
       <Logo />
       <Button onClick={() => setStoreVisible(true)}>스토어</Button>
-      <Button>유저</Button>
+      <Popover opened={opened} onChange={setOpened}>
+        <Popover.Target>
+          <Button onClick={() => setOpened(opened ? false : true)}>유저</Button>
+        </Popover.Target>
+        <Popover.Dropdown>
+          <Button>로그아웃</Button>
+        </Popover.Dropdown>
+      </Popover>
     </nav>
   )
 }
