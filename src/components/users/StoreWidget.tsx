0f75b32f0192852c2b9bcf1263ff9329d908a50e
jeonhyun <jeonhyun@student.42seoul.kr>
2022-11-29 15:38:07 +0900

    style : CSS 가운데정렬

--- src/components/users/StoreWidget.tsx
+++ src/components/users/StoreWidget.tsx
@@ -1,4 +1,4 @@
-import { Image } from '@mantine/core'
+import { Image, Text } from '@mantine/core'
 import { StoreWidgetType } from 'common'
 
 export const StoreWidget = ({
@@ -7,9 +7,15 @@ export const StoreWidget = ({
   widgetData: StoreWidgetType
 }) => {
   return (
-    <div>
-      <Image src={widgetData.image} width='150px'></Image>
-      {widgetData.name + ' : ' + widgetData.discription}
+    <div style={{ textAlign: 'center' }}>
+      <Image
+        src={widgetData.image}
+        width='150px'
+        style={{ display: 'inline-block' }}
+      ></Image>
+      <Text style={{ color: 'white' }}>
+        {widgetData.name + ' : ' + widgetData.discription}
+      </Text>
     </div>
   )
 }
