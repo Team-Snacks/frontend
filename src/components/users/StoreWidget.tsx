795290eee5cb0f88fe665ea75b3eedc040632494
jeonhyun <jeonhyun@student.42seoul.kr>
2022-11-29 16:18:59 +0900

    feat: 드래드 기능 추가

--- src/components/users/StoreWidget.tsx
+++ src/components/users/StoreWidget.tsx
@@ -1,13 +1,23 @@
+import { useDraggable } from '@dnd-kit/core'
 import { Image, Text } from '@mantine/core'
 import { StoreWidgetType } from 'common'
+import { CSS } from '@dnd-kit/utilities'
 
 export const StoreWidget = ({
   widgetData,
 }: {
   widgetData: StoreWidgetType
 }) => {
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({
+    id: widgetData.name,
+  })
+  const style: React.CSSProperties = {
+    transform: CSS.Translate.toString(transform),
+    textAlign: 'center',
+  }
+
   return (
-    <div style={{ textAlign: 'center' }}>
+    <div ref={setNodeRef} style={style} {...listeners} {...attributes}>
       <Image
         src={widgetData.image}
         radius='lg'
