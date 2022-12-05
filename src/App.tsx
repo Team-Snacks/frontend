339e693488188e8a0a58cd8e67f9a1ad0c5f3728
scarf <greenscarf005@gmail.com>
2022-12-05 18:02:54 +0900

    feat: 기본적인 라우팅 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,71 @@
-import { Users } from 'components/users'
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import { LoginPanel, RegisterPanel } from 'components'
+import {
+  Outlet,
+  RouterProvider,
+  Link,
+  createReactRouter,
+  createRouteConfig,
+} from '@tanstack/react-router'
+import { Button } from '@mantine/core'
 
-export const App = () => {
-  return <Users />
+declare module '@tanstack/react-router' {
+  interface RegisterRouter {
+    router: typeof router
+  }
 }
+
+const rootRoute = createRouteConfig()
+
+const indexRoute = rootRoute.createRoute({
+  path: '/',
+  component: () => <div>Index</div>, // FrontPage,
+})
+
+const usersRoute = rootRoute.createRoute({
+  path: '/users',
+  component: () => <div>Users</div>, // Users,
+})
+
+const authRoute = rootRoute.createRoute({
+  path: '/auth',
+  component: RegisterPanel,
+})
+
+const loginRoute = authRoute.createRoute({
+  path: '/login',
+  component: LoginPanel,
+})
+const authRouteConfig = authRoute.addChildren([loginRoute])
+
+const routeConfig = rootRoute.addChildren([
+  indexRoute,
+  usersRoute,
+  authRouteConfig,
+])
+
+const router = createReactRouter({ routeConfig })
+
+export const App = () => (
+  <>
+    <RouterProvider router={router}>
+      <div>
+        <Link to='/'>Home</Link>
+      </div>
+      <Button>
+        <Link to='/'>Home</Link>
+      </Button>
+      <Button>
+        <Link to='/users'>Users</Link>
+      </Button>
+      <Button>
+        <Link to='/auth'>Register</Link>
+      </Button>
+      <Button>
+        <Link to='/auth/login'>Login</Link>
+      </Button>
+      <Outlet />
+      <TanStackRouterDevtools />
+    </RouterProvider>
+  </>
+)
