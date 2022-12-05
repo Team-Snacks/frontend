7d4fccb64c0b9989c5319ade130d76109c112381
scarf <greenscarf005@gmail.com>
2022-12-05 18:30:06 +0900

    refactor: `pos`, `size` 생성자 간략하게 변경 (#32)

--- src/vec2/vec2.ts
+++ src/vec2/vec2.ts
@@ -20,23 +20,15 @@ export const asTuple = (v: Vec2): Tuple => (isPos(v) ? [v.x, v.y] : [v.w, v.h])
 export function pos(size: Size): Pos
 export function pos(x: number, y: number): Pos
 export function pos(a: Size | number, b?: number): Pos {
-  if (typeof a !== 'number' && isSize(a)) {
-    return { x: a.w, y: a.h }
-  } else if (typeof a === 'number' && typeof b === 'number') {
-    return { x: a, y: b }
-  } else {
-    throw new Error(`Invalid arguments (${a}, ${b})`)
-  }
+  return b !== undefined && typeof a === 'number'
+    ? { x: a, y: b }
+    : { x: (a as Size).w, y: (a as Size).h }
 }
 
 export function size(pos: Pos): Size
 export function size(w: number, h: number): Size
 export function size(a: Pos | number, b?: number): Size {
-  if (typeof a !== 'number' && isPos(a)) {
-    return { w: a.x, h: a.y }
-  } else if (typeof a === 'number' && typeof b === 'number') {
-    return { w: a, h: b }
-  } else {
-    throw new Error(`Invalid arguments (${a}, ${b})`)
-  }
+  return b !== undefined && typeof a === 'number'
+    ? { w: a, h: b }
+    : { w: (a as Pos).x, h: (a as Pos).y }
 }
