acacbd2df69a09b7e07a1bfc373c158de812bae2
scarf <greenscarf005@gmail.com>
2022-12-06 14:02:00 +0900

    refactor: 객체 형식 export로 변경

--- src/vec2/operator.ts
+++ src/vec2/operator.ts
@@ -1,20 +1,21 @@
 import { A } from '@mobily/ts-belt'
 import { Vec2, asTuple, Tuple, isPos, pos, size } from './vec2'
 
-type ValueOf<T> = T[keyof T]
-const allOps = ['+', '-', '/', '*'] as const
-type Op = typeof allOps[number]
 export type NumFn<T> = (a: number, b: number) => T
-type OpFns = Record<Op, NumFn<number>>
-const ops: OpFns = {
-  '+': (a, b) => a + b,
-  '-': (a, b) => a - b,
-  '/': (a, b) => a / b,
-  '*': (a, b) => a * b,
+
+const allOps = ['plus', 'sub', 'div', 'mul'] as const
+type Op = typeof allOps[number]
+
+const numOps: Record<Op, NumFn<number>> = {
+  plus: (a, b) => a + b,
+  sub: (a, b) => a - b,
+  div: (a, b) => a / b,
+  mul: (a, b) => a * b,
 } as const
 type VecOps = (a: Vec2, b: Vec2) => Vec2
+
 const vecOps =
-  (fn: ValueOf<OpFns>): VecOps =>
+  (fn: NumFn<number>): VecOps =>
   (a, b) => {
     const val = A.zipWith(asTuple(a), asTuple(b), fn) as Tuple
     return isPos(a) ? pos(...val) : size(...val)
@@ -24,11 +25,13 @@ type VecOpFn = {
   <T extends Vec2>(a: T, b: Vec2): T
 }
 const opGen = (op: Op) => {
-  const fn = vecOps(ops[op])
+  const fn = vecOps(numOps[op])
   return ((a, b) => (b ? fn(a, b) : (c: Vec2) => fn(c, a))) as VecOpFn
 }
-
-export const [plus, sub, div, mul] = allOps.map(opGen)
+export const ops: Record<string, VecOpFn> = Object.fromEntries(
+  allOps.map(op => [op, opGen(op)])
+)
+export const { plus, sub, div, mul } = ops
 
 /** @deprecated 후속 PR에서 제대로 구현 예정 */
 export const round = <T extends Vec2>(v: T): T => {
