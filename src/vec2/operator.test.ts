2105ee29007374254361f91b9c8f800a561111dc
scarf <greenscarf005@gmail.com>
2022-12-06 15:55:59 +0900

    test: 벡터 연산자 및 비교 테스트 추가

--- src/vec2/operator.test.ts
+++ src/vec2/operator.test.ts
@@ -1,17 +1,19 @@
-import { pipe } from '@mobily/ts-belt'
+import { A, pipe } from '@mobily/ts-belt'
 import { expect, test, describe } from 'vitest'
-import { pos, size, isPos, isSize } from './vec2'
-import { plus, round, sub } from './operator'
+import { pos, size, isPos, isSize, Pos } from './vec2'
+import { ops, plus, round, sub } from './operator'
 import { eq } from './compare'
+import { assert, property } from 'fast-check'
+import { posPairGen } from './testUtils'
 
 const [a, b] = [pos(1, 2), size(3, 4)]
 
-test('currying', () => {
+test('커링', () => {
   expect(plus(a, b)).toEqual(pos(4, 6))
   expect(plus(b)(a)).toEqual(pos(4, 6))
 })
 
-test('chaining', () => {
+test('체인', () => {
   expect(pipe(a, plus(b))).toEqual(plus(a, b))
   expect(pipe(a, plus(b), sub(b))).toEqual(a)
   expect(pipe(a, plus(b), sub(b), eq(a))).toBe(true)
@@ -25,6 +27,26 @@ test('vecOps', () => {
   expect(plus(b, a)).toEqual(size(4, 6))
   expect(sub(a, b)).toEqual(pos(-2, -2))
   expect(sub(b, a)).toEqual(size(2, 2))
+})
+
+describe('벡터 연산자(속성 기반)', () => {
+  type VecOpImpl = (a: Pos, b: Pos) => Pos
+  const impl: Record<string, VecOpImpl> = {
+    plus: (a, b) => pos(a.x + b.x, a.y + b.y),
+    sub: (a, b) => pos(a.x - b.x, a.y - b.y),
+    div: (a, b) => pos(a.x / b.x, a.y / b.y),
+    mul: (a, b) => pos(a.x * b.x, a.y * b.y),
+    mod: (a, b) => pos(a.x % b.x, a.y % b.y),
+    round: (a, b) => pos(Math.round(a.x / b.x), Math.round(a.y / b.y)),
+  }
+  const cases = A.zipWith(
+    Object.entries(impl),
+    Object.entries(ops),
+    ([name, impl], [_, op]) => ({ name, impl, op })
+  )
+  test.each(cases)('$name', ({ impl, op }) =>
+    assert(property(posPairGen(), ([a, b]) => eq(op(a, b), impl(a, b))))
+  )
 })
 
 describe('round', () => {
