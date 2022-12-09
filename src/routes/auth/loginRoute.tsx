4083c509d6c09b84a0c60ef75f92bebf6babb5e4
scarf <greenscarf005@gmail.com>
2022-12-09 15:47:33 +0900

    refactor: `routes/`로 이동

---
+++ src/routes/auth/loginRoute.tsx
@@ -0,0 +1,26 @@
+import { Link } from '@tanstack/react-router'
+import { Button, Paper, Stack, Text, Title } from '@mantine/core'
+import { Logo } from 'components/common'
+import { Login, OAuthLogin } from '../../components/login/Login'
+import { authRoute } from 'routes'
+
+/** /auth/login */
+export const loginRoute = authRoute.createRoute({
+  path: '/login',
+  component: () => (
+    <Paper>
+      <Stack align='center'>
+        <Logo />
+        <Title order={2}>로그인</Title>
+        <Login />
+        <>
+          <Text>아직 계정이 없으신가요?</Text>
+          <Link to='/auth'>
+            <Button variant='default'>회원가입</Button>
+          </Link>
+        </>
+        <OAuthLogin />
+      </Stack>
+    </Paper>
+  ),
+})
---
+++ src/routes/auth/registerRoute.tsx
@@ -0,0 +1,26 @@
+import { Link } from '@tanstack/react-router'
+import { Button, Paper, Stack, Text, Title } from '@mantine/core'
+import { Logo } from 'components/common'
+import { OAuthRegister, Register } from '../../components/login/Register'
+import { authRoute } from 'routes'
+
+/** /auth */
+export const registerRoute = authRoute.createRoute({
+  path: '/',
+  component: () => (
+    <Paper>
+      <Stack align='center'>
+        <Logo />
+        <Title order={2}>회원가입</Title>
+        <Register />
+        <>
+          <Text>이미 계정이 있으신가요?</Text>
+          <Link to='/auth/login'>
+            <Button variant='default'>로그인</Button>
+          </Link>
+        </>
+        <OAuthRegister />
+      </Stack>
+    </Paper>
+  ),
+})
