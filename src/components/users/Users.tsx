f836be4cf40653222edd393ef8884eb3fd956c39
jeonhyun <jeonhyun@student.42seoul.kr>
2022-12-23 16:25:48 +0900

    feat: Users 페이지 로그인 안 된 상태에서는 /about으로 이동

--- src/components/users/Users.tsx
+++ src/components/users/Users.tsx
@@ -1,13 +1,19 @@
+import { accessTokenAtom } from 'atoms'
+import { useAtom } from 'jotai'
+import { Navigate } from 'react-router-dom'
 import { Grid } from './Grid'
 import { Navigation } from './Navigation'
 import { Store } from './Store'
 
 export const Users = () => {
+  const [access, setAccess] = useAtom(accessTokenAtom)
+
   return (
     <div>
       <Navigation />
       <Store />
       <Grid />
+      {!access ? <Navigate to='/about' /> : <></>}
     </div>
   )
 }
